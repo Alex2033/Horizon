dc621bfd18c095cbd3554bddf824126a7575f872
Александр <XoxlofAlexandr@yandex.ru>
2018-10-04 11:39:48 +0300

    Сделана навигация на десктопное разрешение, сделано выпадающее меню для элементов навигации

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,15 @@
-$(function() {
+$(document).ready(function() {
 
-	// Custom JS
+
+    $('.nav-button').click(function (){
+        $('.subnav').toggleClass('active');
+    });
+
+
+    $(".tabs__content").not(":first").hide();
+    $(".tabs__item").click(function() {
+        $(".tabs__item").removeClass("active").eq($(this).index()).addClass("active");
+        $(".tabs__content").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active");
 
 });
