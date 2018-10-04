493dd12e648bc697c56e39e23ad5687f944afc4e
Александр <XoxlofAlexandr@yandex.ru>
2018-10-04 23:33:37 +0300

    Сверстана всплывающее окно поиска, всплывающие меню каталога и элементов навигации

--- app/js/common.js
+++ app/js/common.js
@@ -12,4 +12,17 @@ $(document).ready(function() {
         $(".tabs__content").hide().eq($(this).index()).fadeIn()
     }).eq(0).addClass("active");
 
+    $('.navigation-search__button').click(function() {
+
+        $('.navigation-search__input').css({
+            'opacity': '1',
+            'visibility': 'visible',
+            'width': $('.navigation__container').width() - $('.nav-button').width() - $('.navigation-search__button').width() - 24 + 'px'
+        });
+
+        setTimeout(() => {
+            $(this).prop('type', 'submit');
+        }, 300);
+    });
+
 });
